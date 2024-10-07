417b1af66f26640a28985434c52b3db5b4437c2b
humberto <matheus.oliveira@sysmap.com.br>
2024-10-06 20:15:37 -0400

    feat(routes): Creates a redirection from the main page to the login

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,3 @@
-import type { MetaFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
-export default function Index() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-        Welcome to Linchat
-      </h1>
-    </div>
-  );
-}
-
+export const loader = () => redirect("/login")
